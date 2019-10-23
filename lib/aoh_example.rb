778aa0ab11277dec63eede8eaa97ce91a7d4c224
David Jaworski <jaws360@gmail.com>
2019-10-23 20:51:01 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -3,7 +3,7 @@
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
-KATE_G = { name:"Kate Gompert", occupation:"None"},
+KATE_G = { name:  "Kate Gompert", occupation: "None"},
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
@@ -19,7 +19,7 @@ def literal_aoh
   { name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" }, 
   { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:"Kate Gompert", occupation:"None"},
+  { name:  "Kate Gompert", occupation: "None"},
   { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 end
